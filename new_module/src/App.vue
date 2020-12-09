52b7931d0ed9894a08732bf5730947cbf95406e7
nguyenduchuyyy <nguyenduchuyyy@gmail.com>
2020-12-09 10:17:14 +0700

    create new module and practice with slot

---
+++ new_module/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <app-card card-words="meow">
+            <p>hihihi</p>
+          </app-card>
+        </div>
+      </div>
+    </div>    
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue"
+
+export default {
+  components:{
+    'app-card':Card,
+  },
+  data: function() {
+    return{
+      
+    }
+  },
+  methods:{
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ new_module/src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{cardWords}}</h2>
+        <h2>Declared</h2>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['cardWords']
+}
+</script>
+
+<style scoped>
+    div{
+        border: 1px solid black;
+        text-align: center;
+        box-shadow: 1px 1px 2px rebeccapurple;
+        padding: 30px;
+        margin: 20px;
+    }
+</style>
