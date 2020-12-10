e2d8e1d9738d525cc58964f5cf1a36dc420231b7
nguyenduchuyyy <nguyenduchuyyy@gmail.com>
2020-12-10 09:54:40 +0700

    Directive customize

--- mini-project/src/Directives.vue
+++ mini-project/src/Directives.vue
@@ -8,29 +8,62 @@
       </div>
       <div class="col-md-4 offset-md-4">
         <h1>Customize in derectives</h1>
-        <p v-momo="'red'">DDerectivesderectivesderectives</p>
+        <p v-momo>DDerectivesderectivesderectives</p>
+        <p v-moto="'red'">Moto</p>
+        <p v-custom:background.delayed="'red'">agragragragragr</p>
+        <p v-local:background.delayed.blink="{ main: 'violet' }">
+          agragragragragr
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  data: function(){
-    return{
-     
-    }
+  data: function() {
+    return {};
   },
-  components: {
-  },
+  components: {},
   methods: {
-    ss(){
-    }
+    ss() {}
   },
-}
+  directives: {
+    local: {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 1000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.main;
+          let secondColor = "green";
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.agr != "background") {
+                el.style.color = currentColor;
+              } else {
+                el.style.backgroundColor = currentColor;
+              }
+            });
+          }, 800);
+        } else {
+          setTimeout(() => {
+            if (binding.agr != "background") {
+              el.style.color = binding.value;
+            } else {
+              el.style.backgroundColor = binding.value;
+            }
+          }, delay);
+        }
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- mini-project/src/main.js
+++ mini-project/src/main.js
@@ -5,10 +5,32 @@ import Directives from './Directives.vue'
 
 Vue.directive('momo', {
   bind(el, binding, vnode) {
+    el.style.color = 'green';
+  }
+});
+Vue.directive('moto', {
+  bind(el, binding, vnode) {
     el.style.color = binding.value;
   }
 });
+Vue.directive('custom', {
+  
+  bind(el, binding, vnode) {
+    var delay = 0;
+    if (binding.modifiers['delayed']){
+      delay = 1000;
+    } 
+    setTimeout(()=>{
+      if (binding.agr != 'background') {
+        el.style.color = binding.value;
+      } else {
+        el.style.backgroundColor = binding.value;
+      }
+    },delay);
 
+    
+  }
+});
 new Vue({
   el: '#app',
   render: h => h(Directives)
