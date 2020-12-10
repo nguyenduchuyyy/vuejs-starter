a147fe58a7af849c2931047690d9d20120ea8a4e
nguyenduchuyyy <nguyenduchuyyy@gmail.com>
2020-12-10 08:25:50 +0700

    Directive in Vuejs

---
+++ mini-project/src/Directives.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 offset-md-4">
+        <h1>Build in derectives</h1>
+        <p v-text="'Something `bout you'"></p>
+        <p v-html="'<h2>Something `bout you</h2>'"></p>
+      </div>
+      <div class="col-md-4 offset-md-4">
+        <h1>Customize in derectives</h1>
+        <p v-momo="'red'">DDerectivesderectivesderectives</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: function(){
+    return{
+     
+    }
+  },
+  components: {
+  },
+  methods: {
+    ss(){
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
--- mini-project/src/main.js
+++ mini-project/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 // import App from './App.vue'
-import Form from './Form.vue'
+// import Form from './Form.vue'
+import Directives from './Directives.vue'
 
+Vue.directive('momo', {
+  bind(el, binding, vnode) {
+    el.style.color = binding.value;
+  }
+});
 
 new Vue({
   el: '#app',
-  render: h => h(Form)
+  render: h => h(Directives)
 })
